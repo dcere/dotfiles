0a478352257fe4efdf80b946ddca6a8972e32f52
dcere <davidzgz123@gmail.com>
2025-03-07 10:24:08 +0100

    Update wezterm.lua
    Remove force autoreloading

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,9 +6,6 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- Add config file to watch list so that wezterm is reloaded when we change this config
-wezterm.add_to_config_reload_watch_list("$HOME/.wezterm.lua")
-
 -- Bash as interactive shell. Ubuntu 22 fix.
 config.default_prog = { 'bash', '-i' }
 
